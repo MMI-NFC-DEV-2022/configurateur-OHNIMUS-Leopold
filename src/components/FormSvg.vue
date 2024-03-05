5298ec7e7efc0c05969fa0a6872fd535c50b4e3e
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-03-05 14:21:09 +0100

    formulaire

---
+++ src/components/FormSvg.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {ref} from "vue"
+
+import SvgProfil from '@/components/SvgProfil.vue';
+import SvgDessus from '@/components/SvgDessus.vue';
+
+const props = defineProps<{
+  data?: Basket;
+  id?: string;
+}>();
+
+const chaussure = ref<Basket>(props.data ?? {});
+</script>
+
+<template>
+<div class="p-2">
+
+    <ul class="flex gap-1">
+        <li>
+            <a href="">Profil</a>
+            <SvgProfil class="carrousel-item w-64" v-bind="chaussure" id="profil"></SvgProfil>
+        </li>
+            <li>
+            <a href="">Dessus</a>
+            <SvgDessus class="carrousel-item w-64" v-bind="chaussure" id="dessus"></SvgDessus>
+        </li>
+    </ul>
+
+
+    <FormKit type="form" v-model="chaussure" >
+        <FormKit name="semelle" label="semelle" value="#ffffff" type="color"></FormKit>
+        <FormKit name="empeigne" label="empeigne" value="#ffffff" type="color"></FormKit>
+        <FormKit name="pointe" label="pointe" value="#ffffff" type="color"></FormKit>
+        <FormKit name="oeillet" label="oeillet" value="#ffffff" type="color"></FormKit>
+        <FormKit name="bande" label="bande" value="#ffffff" type="color"></FormKit>
+        <FormKit name="languette" label="languette" value="#ffffff" type="color"></FormKit>
+        <FormKit name="lacet" label="lacet" value="#ffffff" type="color"></FormKit>
+        <FormKit name="trimestre" label="trimestre" value="#ffffff" type="color"></FormKit>
+    </FormKit>
+</div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import SvgProfil from '@/components/SvgProfil.vue';
-import SvgDessus from '@/components/SvgDessus.vue';
+import FormSvg from '@/components/FormSvg.vue';
 </script>
 
 <template>
   <div>
     <h1>This is an home page test</h1>
   </div>
-  <SvgDessus />
-  <SvgProfil />
+  <FormSvg />
 </template>
