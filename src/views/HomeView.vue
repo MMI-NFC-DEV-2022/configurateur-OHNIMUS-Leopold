0271e9a42b937fecbc609f192fd9192c4d06045b
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-03-05 14:10:21 +0100

    dessus de la chaussure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import SvgProfil from '@/components/SvgProfil.vue';
+import SvgDessus from '@/components/SvgDessus.vue';
 </script>
 
 <template>
   <div>
     <h1>This is an home page test</h1>
   </div>
+  <SvgDessus />
   <SvgProfil />
 </template>
