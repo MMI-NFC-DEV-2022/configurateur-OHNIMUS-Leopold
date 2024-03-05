b9952f97c21b40e593498e133902e211ea4d59eb
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-03-05 15:17:11 +0100

    formulaire images

---
+++ src/components/Images.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { ChaussureImage } from '@/types-images';
+
+withDefaults(defineProps<ChaussureImage>(), {
+    orientation: 'cote',
+    bandes: 'bleu',
+    cuir: 'bleu',
+    mousse: 'bleu'
+})
+
+</script>
+
+<template>
+    <div class="relative">
+        <img :src="`/images/${orientation}/base.png`" class="h-auto w-full" alt="">
+
+    </div>
+</template>
